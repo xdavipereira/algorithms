3ac773d0caa1b362012f0653cc053bad125bfb6b
Davi Pereira dos Santos <davi.santos@MVFOR.local>
2020-10-13 14:33:26 -0300

    add: binary search

---
+++ binary-search.js
@@ -0,0 +1,31 @@
+function binarySearch(arr, item) {
+    
+
+    if(!arr) return null;
+
+    let low = 0;
+    let high = arr.length - 1;
+
+    while(low <= high) {
+        let mid = Math.round(low + high);
+        let guess = arr[mid];
+
+        if(guess === item) {
+            return mid;
+        }
+    
+        if(guess > item){
+            high = mid - 1;
+        } else {
+            low = mid +1
+        }
+        
+    }
+    
+    return null;
+    
+}
+
+const x = binarySearch([1,2,3,4,5, 6, 7, 8, 9, 10], 9);
+
+console.log(x);
